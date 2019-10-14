c080d315417b4b11589f4db2fbf59476d6cfe127
Shanti42 <smp163@case.edu>
2019-10-14 17:44:40 -0400

    Add files

---
+++ src/JobNode.java
@@ -0,0 +1,75 @@
+import java.util.Objects;
+
+//Class JobNode {
+//is comparable by endtime
+public class JobNode implements Comparable {
+
+    // Attributes for:
+    // name
+    String name;
+    // start
+    int start;
+    // end
+    int end;
+    // priority
+    int priority;
+    //JobNode predecessor
+    JobNode predecessor = null;
+
+    //assign name, start, end and priority on creation
+    private JobNode(String name, int start, int end, int priority) {
+        this.name = name;
+        this.start = start;
+        this.end = end;
+        this.priority = priority;
+    }
+
+    public static JobNode of(String name, int start, int end, int priority) {
+        Objects.requireNonNull(name, "JobNode, of() -> name is null");
+        //start time must be before end time
+        assert (start < end);
+        //priority must be between 1 and 100 inclusive
+        assert (1 <= priority && priority <= 100);
+        return new JobNode(name, start, end, priority);
+    }
+
+
+    //public getters for all attributes
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public JobNode getPredecessor() {
+        return predecessor;
+    }
+
+    //public setter for predecessor
+    public void setPredecessor(JobNode predecessor) {
+        this.predecessor = predecessor;
+    }
+
+    @Override
+    public int compareTo(Object other) {
+        Objects.requireNonNull(other, "JobNode, compareTo -> input parameter to compare is null");
+        if(other instanceof JobNode){
+            JobNode otherNode = (JobNode)other;
+            return Integer.compare(this.end, otherNode.end);
+        } else {
+            throw new IllegalArgumentException("JobNode, compareTo -> Object of wrong type given, expected type JobNode");
+        }
+
+    }
+}
---
+++ src/Scheduler.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Scheduler {
+
+    // Pre-condition: There is a list of nodes with names, priorities, start and end times, priority of nodes must be between 1 and 100
+    // Post-condition: There will be a schedule of nodes that maximize the total priority
+
+    // Input:  List of nodes
+    // Output: A schedule of nodes that maximizes the benefit
+
+    // findOptimalSchedule( List of JobNodes ) {
+    public List<JobNode> findOptimalSchedule(ArrayList<JobNode> nodeList) {
+
+        // List sortedNodes <- Sort input list by endTime (early to late) using merge sort
+        List<JobNode> sortedNodes = new ArrayList<>(nodeList);
+        Collections.sort(sortedNodes);
+        // Assign predecessor nodes for each node
+        assignPredecessors(sortedNodes);
+
+        // benefits <- array with length equal to the number of nodes, represents total benefits at each Node
+        int[] totalBenefits = new int[sortedNodes.size()];
+
+        // set first element of benefit array to 0
+        totalBenefits[0] = 0;
+
+        /**will require specificly an array list **/
+
+        // for all nodes starting at the first node in the sorted list
+        for (int i = 0; i < nodeList.size(); i++) {
+            // Total benefit in array corresponding to current node <- max(benefit at previous job node, benefit at predecessor node + benefit of current node)
+            /**TO DO**/
+            //totalBenefits[i] = to
+        }
+
+        // return createSchedule(benefits, sortedNodes)
+        return createSchedule(totalBenefits, sortedNodes);
+
+    }
+
+    // void assignPredecessors(sorted list of nodes) -> returns the predecessor node which does not overlap
+    private void assignPredecessors(List<JobNode> nodeList) {
+        /** TO DO **/
+        // Array A <- empty array with n spots where n is the length of list
+        // For each JobNode in list:
+        // st <- start time of this JobNode
+        // for each node in sorted list, going in reverse order, until condition is met:
+        // If end time of checked node < st
+        // assign checked node as predecessor of this JobNode and continue main loop
+
+    }
+
+    // schedule <- empty list, will contain the optimal schedule of nodes
+    private List<JobNode> createSchedule(int[] totalBenifits, List<JobNode> sortedNodes) {
+        /** TO DO **/
+        // loop backwards over list of total benefits for each node until the first node is reached:
+        // If the total benefit at a node “A”  is equal to the total benefit at the previous node “B”, then node “A” is not included in the schedule.
+        //
+        // If the total benefit of the node is equal to the benefit of the node plus the benefit of the predecessor, then that node is included. Add that node to the schedule.
+
+        //return schedule
+        return null;
+    }
+
+
+
+
+
+
+    }
+
+
+
+
+
